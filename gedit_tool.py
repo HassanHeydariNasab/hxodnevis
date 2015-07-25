eef069d5c7db800dff06be5dd0f376a9caba307e
hsn6 <hsn6@openmailbox.org>
2015-07-25 22:52:57 +0430

    version 0.9

--- gedit_tool.py
+++ gedit_tool.py
@@ -5,6 +5,8 @@ import sys
 import os
 from peewee import *
 import subprocess
+from random import random
+
 db = SqliteDatabase('%s/hxodnevis/wordsf.db'%(os.environ.get('HOME')))
 db.connect()
 class Word(Model):
@@ -24,13 +26,14 @@ for i in f_u:
 for w in sorted(f2, key=f2.get, reverse=True):
     #print w, f2[w]
     p.append(w)
-if len(p) == 0:
+len_p = len(p)
+if len_p == 0:
     pass
-elif len(p) == 1:
+elif len_p== 1:
     #p[0] = p[0][0:-1]
     sys.stdout.write(" ")
     sys.stdout.write(p[0])
-elif len(p) == 2:
+elif len_p == 2:
     c = "gxmessage -encoding utf-8 -print -center -buttons '%s','%s' 'انتخاب کنید'"%(p[0].decode('utf-8'), p[1].decode('utf-8'))
     pp = subprocess.Popen(c, stdout=subprocess.PIPE, shell=True)
     (output, err) = pp.communicate()
@@ -38,7 +41,30 @@ elif len(p) == 2:
     sys.stdout.write(" ")
     sys.stdout.write(output)
 else:
-    c = "gxmessage -encoding utf-8 -print -center -buttons '%s','%s',' %s' 'انتخاب کنید'"%(p[0].decode('utf-8'), p[1].decode('utf-8'), p[2].decode('utf-8'))
+    r1 = int(random()*2)
+    if len_p > 4:
+        r2 = int(random()*5)
+        while r2 == r1:
+            r2 = int(random()*5)
+        r3 = int(random()*len_p)
+        while r3 == r1:
+            r3 = int(random()*len_p)
+        while r3 == r2:
+            r3 = int(random()*len_p)
+    elif len_p == 3:
+        r1 = 0
+        r2 = 1
+        r3 = 2
+    elif len_p == 4:
+        r2 = int(random()*4)
+        while r2 == r1:
+            r2 = int(random()*4)
+        r3 = int(random()*4)
+        while r3 == r1:
+            r3 = int(random()*4)
+        while r3 == r2:
+            r3 = int(random()*4)
+    c = "gxmessage -encoding utf-8 -print -center -buttons '%s','%s',' %s' 'انتخاب کنید'"%(p[r1].decode('utf-8'), p[r2].decode('utf-8'), p[r3].decode('utf-8'))
     pp = subprocess.Popen(c, stdout=subprocess.PIPE, shell=True)
     (output, err) = pp.communicate()
     output = output[0:-1]
--- parser.py
+++ parser.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*- 
 from __future__ import unicode_literals
 from peewee import *
-from HTMLParser import HTMLParser
+from bs4 import BeautifulSoup, NavigableString
 from hazm import *
-
 db = SqliteDatabase("wordsf.db")
 db.connect()
 class Word(Model):
@@ -16,32 +15,18 @@ class Word(Model):
 f = open("html/f.html")
 fr = f.read()
 f.close()
-global word_list
-word_list = [] 
-class MyHTMLParser(HTMLParser):
-    def print_p_contents(self, html):
-        self.tag_stack = []
-        self.feed(html)
-
-    def handle_starttag(self, tag, attrs):
-        self.tag_stack.append(tag.lower())
-
-    def handle_endtag(self, tag):
-        self.tag_stack.pop()
-
-    def handle_data(self, data):
-        print len(self.tag_stack)
-        if self.tag_stack[-1] == 'p':
-            data_d = data.decode("utf-8")
-            data_t = word_tokenize(data_d)
-            global word_list
-            for i in data_t:
-                word_list.append(i)
-                word_list.append(i)
-        print word_list
-parser = MyHTMLParser()
-#parser.feed(fr)
-parser.print_p_contents('<p class="rtejustify"> <div> test </div> </p>')
-#for i in range(1, len(word_list)-1, 2):
-    #Word(word1 = word_list[i], word2 = word_list[i+1]).save()
+word_list1 = []
+word_list2 = []
+soup = BeautifulSoup(fr)
+all_p = soup.find_all("p")
+for i in all_p:
+    if type(i.string) is not type(None):
+        word_list = word_tokenize(i.string)
+        for w in word_list:
+            word_list1.append(w)
+for i in word_list1:
+    word_list2.append(i)
+    word_list2.append(i)
+for i in range(1, len(word_list2)-1, 2):
+    Word(word1 = word_list2[i], word2 = word_list2[i+1]).save()
 db.close()
